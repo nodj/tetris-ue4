7e2b800e40eabb36f9574d2646824a1f1d87e7c0
johan <johan.duparc@gmail.com>
2019-12-16 23:07:45 -0500

    update to 4.24

--- Source/Tetris.Target.cs
+++ Source/Tetris.Target.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 
 public class TetrisTarget : TargetRules
 {
-    public TetrisTarget(TargetInfo Target) : base(Target)
+	public TetrisTarget(TargetInfo Target) : base(Target)
 	{
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
 		Type = TargetType.Game;
 		ExtraModuleNames.Add("Tetris");
 
-        bUseUnityBuild = true;
-    }
+		bUseUnityBuild = true;
+	}
 }
--- Source/TetrisEditor.Target.cs
+++ Source/TetrisEditor.Target.cs
@@ -5,9 +5,13 @@ using System.Collections.Generic;
 
 public class TetrisEditorTarget : TargetRules
 {
-    public TetrisEditorTarget(TargetInfo Target) : base(Target)
+	public TetrisEditorTarget(TargetInfo Target) : base(Target)
 	{
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
 		Type = TargetType.Editor;
 		ExtraModuleNames.Add("Tetris");
+
+		bUseUnityBuild = false;
 	}
 }
